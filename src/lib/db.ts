b8abafc177d2e5e36879a5ba87bc39c686e72ad8
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-06-23 00:55:59 +0530

    add pg database support

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import "server-only"; // Ensures this code only runs on the server.
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set. Ensure it exists in the root .env");
+}
+
+export const pool = new Pool({
+  connectionString: databaseUrl,
+});
